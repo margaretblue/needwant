8a23d8a52f565ad4a65168e72b6347983dde4eca
Margaret Morris <margaretblue@gmail.com>
2013-10-19 15:51:54 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Needwant::Application.config.secret_token = '49ad305832ba26c24f5e0b0b76b2d20ffc4ea21a4ab57e19ecf38b822600f72c3ef07641429fcfefa19b26151f4824cc467a91d2dda2bafaa3d79a1cc2bfc2ac'
